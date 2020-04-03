1eb907fef32732ba57072c6bc8039f024cadff12
maadc <markus@schranz.io>
2020-04-03 22:51:39 +0200

    news werden als ion-card dargestellt mit refresh

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { HttpHeaders } from "@angular/common/http";
 import { stringify } from "querystring";
+import { CompileTemplateMetadata } from '@angular/compiler';
 
 @Component({
   selector: "app-home",
@@ -10,17 +11,18 @@ import { stringify } from "querystring";
 })
 export class HomePage {
   title: string = "Home";
+  news: object;
 
   constructor(private http: HttpClient) {}
 
-  fire() {
-    console.log(
+  fire(event) {
       this.postRequest().subscribe((news: any) => {
         console.log(news);
-
         this.title = news.data[0].title;
+        this.news= news.data;
+        event.target.complete()
       })
-    );
+      
   }
   postRequest() {
     let url: string = "https://schranz.io/api/list?entity=news";
@@ -28,7 +30,7 @@ export class HomePage {
       .set("Content-Type", "application/json")
       .set(
         "APPCMS-TOKEN",
-        ""
+        "75dfd37f92d7c02700c33a91bc8aebc1ce76dc644615e5954862b9dd3f75752631ef9ec4be30443c8e6ec7ba378eed6f3d4ba5fe3c46d05d8c1f89b347425859"
       );
     return this.http.post(url, {}, {headers, responseType: 'json'});
   }
---
+++ src/app/home/news.processing.ts
@@ -0,0 +1,5 @@
+export class NewsProcessing{
+    static contentToHtml(string){
+
+    }
+}
