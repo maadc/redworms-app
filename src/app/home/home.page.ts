5f921dcb8de0642b8fdd8837d72e292d750d34c0
maadc <markus@schranz.io>
2020-04-05 17:53:36 +0200

    API-Call für Gilden-Ankündigung + Design

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,43 +1,56 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { HttpHeaders } from "@angular/common/http";
-import { stringify } from "querystring";
-import { CompileTemplateMetadata } from '@angular/compiler';
 
-import { tokens } from '../../../private-tokens'
+import { tokens } from "../../../private-tokens";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.page.html",
   styleUrls: ["./home.page.scss"],
 })
-export class HomePage implements OnInit{
-  title: string = "Home";
+export class HomePage implements OnInit {
+  title: string = "News";
   news: object;
+  ankundigung: string = "";
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.postRequest().subscribe((news: any) => {
-      this.news= news.data;
-    })
+    this.postRequest("https://schranz.io/api/list?entity=news").subscribe(
+      (news: any) => {
+        this.news = news.data;
+      }
+    );
+
+    this.postRequest("https://schranz.io/api/list?entity=seiten").subscribe(
+      (content: any) => {
+        content.data.forEach(e => {
+          if (
+            e.id == "b1e337cc-7742-11ea-9494-4061862b9065" &&
+            e.title == "Ankündigung"
+          ) {
+            console.log(e.content);
+            this.ankundigung = e.content;
+          }
+        });
+      }
+    );
+  }
+
+  private newMethod() {
+    return this;
   }
 
   reload(event) {
-      this.postRequest().subscribe((news: any) => {
-        this.news= news.data;
-        event.target.complete()
-      })
-      
+    this.ngOnInit();
+    event.target.complete();
   }
-  postRequest() {
-    let url: string = "https://schranz.io/api/list?entity=news";
+
+  postRequest(url: string) {
     const headers = new HttpHeaders()
       .set("Content-Type", "application/json")
-      .set(
-        "APPCMS-TOKEN",
-        tokens.APPCMSTOKEN
-      );
-    return this.http.post(url, {}, {headers, responseType: 'json'});
+      .set("APPCMS-TOKEN", tokens.APPCMSTOKEN);
+    return this.http.post(url, {}, { headers, responseType: "json" });
   }
 }
